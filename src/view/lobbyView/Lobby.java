62217de0c13e90c29fc2eb59e114a9feb940cc13
mingi <meengi07@gmail.com>
2021-06-20 12:49:42 +0900

    commit

--- src/view/lobbyView/Lobby.java
+++ src/view/lobbyView/Lobby.java
@@ -1,8 +1,10 @@
 package view.lobbyView;
 
-import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -14,9 +16,7 @@ import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-import view.lobbyView.*;
-
-public class Lobby extends JFrame{
+public class Lobby extends JFrame implements ActionListener{
 	// -----------------기본 설정 --------------------
 	private JPanel panel, inputChat;
 	private JFrame frame;
@@ -40,21 +40,7 @@ public class Lobby extends JFrame{
 	private ImageIcon myPageImg = new ImageIcon(Main.class.getResource("../images/mypageImg.png"));
 	private ImageIcon reImg = new ImageIcon(Main.class.getResource("../images/reImg.png"));
 	
-	//배경이미지 삽입 메소드 
-	public void paint(Graphics g) {
-		//background = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
-		//screenGraphic = background.getGraphics();
-		//screenDraw(screenGraphic);
-		g.drawImage(background, 0, 0, null);
-		
-	}
-	/*
-	public void screenDraw(Graphics g) {
-		g.drawImage(introBackground, 0, 0, null);
-		paintComponents(g);
-		this.repaint();
-	}
-	*/
+	
 	//기본생성자 
 	public Lobby() {
 		LobbyMain();
@@ -62,20 +48,19 @@ public class Lobby extends JFrame{
 	
 	//메인                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
 	public void LobbyMain() {
-			add(panel);
+			
+			//frame = new JFrame();
+			
 //---------------메인 프레임 패널설정----------------------// 
-			setTitle("로비");
+			
 			setUndecorated(true);
-			setVisible(true);
 			setSize(1280,720);
 			setResizable(false);
 			setLocationRelativeTo(null);
-			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-					
+			
 			//Music caMusic = new Music("bgm.mp3",true);
 			//caMusic.start();		
-			
-			
+		
 //--------------------버튼설정-------------------------//
 			//게임생성버튼 
 			createGame = new JButton(createImg);
@@ -86,7 +71,7 @@ public class Lobby extends JFrame{
 			createGame.setFocusPainted(false);
 			
 			//상점버튼 
-			shop = new JButton(createImg);
+			shop = new JButton(shopImg);
 			shop.setLayout(null);
 			shop.setBounds(340, 50, 200, 100); //상점버튼 
 			shop.setBorderPainted(false);
@@ -94,7 +79,7 @@ public class Lobby extends JFrame{
 			shop.setFocusPainted(false);
 			
 			//마이페이지버튼 
-			myPage = new JButton(createImg);
+			myPage = new JButton(myPageImg);
 			myPage.setLayout(null);
 			myPage.setBounds(590, 50, 200, 100);
 			myPage.setBorderPainted(false);
@@ -102,7 +87,7 @@ public class Lobby extends JFrame{
 			myPage.setFocusPainted(false);
 			
 			//친구추가버튼 
-			relationship = new JButton(createImg);
+			relationship = new JButton(reImg);
 			relationship.setLayout(null);
 			relationship.setBounds(840, 50, 200, 100); 
 			relationship.setBorderPainted(false);
@@ -116,42 +101,85 @@ public class Lobby extends JFrame{
 		
 			
 //--------------방 리스트----------------------
-			
+			// JTable roomList, userList, ranking;
+			roomList = new JTable();
 			roomScroll = new JScrollPane(roomList);
-			roomScroll.setBounds	(90, 210, 900, 200); // 방 리스트 
+			//roomList.setBounds(50, 50, 900, 200); // 방 리스트
+			roomScroll.setPreferredSize(new Dimension(900,200));
+			roomScroll.setLocation(50, 200);
 			
 //------------유저리스트, 랭킹리스트--------------
-			/*
-			userScroll.setBounds	(1050, 210, 150, 200); // 유저접속리스트 
-			rankScroll.setBounds	(1050, 470, 150, 200); // 유저랭킹 
+			userList = new JTable();
+			ranking = new JTable();
 			
 			userScroll = new JScrollPane(userList);
 			rankScroll = new JScrollPane(ranking);
-			*/
+			userScroll.setBounds(1050, 210, 150, 200); // 유저접속리스트 
+			rankScroll.setBounds(1050, 470, 150, 200); // 유저랭킹
+			//userScroll.setPreferredSize(new Dimension(1000,100));
+			//userList.setBounds(1050,210,150,200);
+			
 //--------------채팅창-------------------//
+			
+			chat = new JTextArea(); 
+			chat.setEditable(false);
+			chat.setBounds(90, 450, 900, 200);
+			
 			chatScroll = new JScrollPane(chat);
-			//inputChat.add(input);
-			//inputChat.setVisible(true);
+			
+			input = new JTextField(30);
+			input.addActionListener(this);
+			input.setBounds(90, 550, 900, 100);
+			
+			/*
+			chatScroll = new JScrollPane(chat);
+			inputChat = new JPanel();
+			inputChat.add(input);
+			inputChat.setVisible(true);
 			
 			chatScroll.setBounds	(90, 470, 900, 200); // 채팅창 
-			//inputChat.setBounds		(90, 470, 900, 200); // 인풋 
-			
+			inputChat.setBounds		(90, 470, 900, 200); // 인풋 
+			*/
 			
 //--------------패널 붙이기 ---------------//
 			
+			panel = new JPanel();
 			
-			panel.add(inputChat);
-			panel.add(createGame);
-			panel.add(shop);
-			panel.add(myPage);
-			panel.add(relationship);
+			//panel.add(createGame);
+			//panel.add(shop);
+			//panel.add(myPage);
+			//panel.add(relationship);
 			panel.add(roomScroll);
-			panel.add(chatScroll);
-			panel.add(userScroll);
-			panel.add(rankScroll);
+			//panel.add(chatScroll);
+			//panel.add(userScroll);
+			//panel.add(rankScroll);
+			//panel.add(input);
 			
+			add(panel);
+			setVisible(true);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		}
+	//배경이미지 삽입 메소드 
+		public void paint(Graphics g) {
+			//background = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+			//screenGraphic = background.getGraphics();
+			//screenDraw(screenGraphic);
+			g.drawImage(background, 0, 0, null);
+			
+		}
 		
+		public void screenDraw(Graphics g) {
+			g.drawImage(introBackground, 0, 0, null);
+			paintComponents(g);
+			this.repaint();
+		}
+		
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == input) {
+				chat.append(input.getText() + "\n");
+				input.selectAll();
+			}
+		}
 			
 }
